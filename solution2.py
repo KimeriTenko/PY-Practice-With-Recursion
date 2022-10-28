4049825eadf33ea4cc7161811062e6908255465d
Mary Frye <deimospho@gmail.com>
2022-10-27 20:44:57 -0400

    INITIAL commit

--- solution2.py
+++ solution2.py
@@ -1,1 +1,13 @@
 # Write code for algorithm 2 below
+# Write a function that prints the natural numbers from 1 to n.
+
+def natural_numbers(x,i):
+	if x > i:
+        return
+    else:
+        print(x)
+        natural_numbers(x + 1, i)
+
+n=10
+natural_numbers(1,n)
+#output: 1 2 3  
--- solution3.py
+++ solution3.py
@@ -1,1 +1,18 @@
 # Write code for algorithm 3 below
+#We want to define a function that takes a number, let's say n , 
+# prints it, and calls itself again with the value of n-1. 
+# The function will keep calling itself until the base case is met. 
+# One way to do this is to determine if the number is equal to 0 and when it is, stop the function.
+# Think about what the base case and recursive case would be.
+
+def count_down(n):
+  #inherent base case
+  #(will stop if n <= 0)
+  if n>0:
+      #recursive case
+      print(n)
+      count_down(n-1)
+   
+#test case
+n=8
+count_down(n)
